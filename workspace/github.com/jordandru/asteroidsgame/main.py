6bf4129333a6475b78ea3c792813f83e2a3ff868
jordandru <jordansboxing@gmail.com>
2025-06-17 11:12:25 -0600

    Add asteroid classes and ignore virtual environment

--- workspace/github.com/jordandru/asteroidsgame/main.py
+++ workspace/github.com/jordandru/asteroidsgame/main.py
@@ -1,6 +1,7 @@
 import pygame
 from player import Player
 from constants import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,18 +12,26 @@ def main():
     time_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
     my_player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        my_player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        my_player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         dt = time_clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
