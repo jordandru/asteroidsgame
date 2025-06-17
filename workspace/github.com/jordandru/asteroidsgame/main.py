a770ed79aa020dccf23f6089456ae561378af945
jordandru <jordansboxing@gmail.com>
2025-06-17 09:31:34 -0600

    Added Player (Rotation & Movement)

--- workspace/github.com/jordandru/asteroidsgame/main.py
+++ workspace/github.com/jordandru/asteroidsgame/main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from player import Player
 from constants import *
 
 def main():
@@ -8,14 +8,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    time_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    my_player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        my_player.update(dt)
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
+        dt = time_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
