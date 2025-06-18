fbeb8cbe81e1bc079570526d29df6a08851a9d99
jordandru <jordansboxing@gmail.com>
2025-06-17 21:33:08 -0600

    Shooting Rate adjusted

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,6 +19,7 @@ class Player(CircleShape):
         self.rotation = self.rotation + (PLAYER_TURN_SPEED * dt)
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -39,5 +41,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "blue", self.triangle())
 
     def shoot(self):
-        shooting = Shot(self.position.x, self.position.y)
-        shooting.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.timer <= 0:
+            shooting = Shot(self.position.x, self.position.y)
+            shooting.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
